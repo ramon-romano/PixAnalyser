b94781380f9dad052bafa32eed695df381975d50
MatheusHAB <152932283+MatheusHAB@users.noreply.github.com>
2025-06-17 01:58:16 -0300

    :sparkles: feat: PixKeyInfoScreen

--- src/App.jsx
+++ src/App.jsx
@@ -9,17 +9,19 @@ import PixTransactionScreen from "../src/components/PixTransactionScreen/PixTran
 import PixAccountScreen from "../src/components/PixAccountScreen/PixAccountScreen";
 import PixConfirmationScreen from "../src/components/PixConfirmationScreen/PixConfirmationScreen";
 import PixSuccessfulScreen from "../src/components/PixSuccessfulScreen/PixSuccessfulScreen";
+import PixKeyInfoScreen from "../src/components/PixKeyInfoScreen/PixKeyInfoScreen";
 
 function App() {
   return (
     <Router>
       <Routes>
         <Route path="/" element={<Navigate to="/home" replace />} />
-        <Route path="/home" element={<PixInputScreen />} />
+        <Route path="/home" element={<PixKeyInfoScreen />} />
         <Route path="/valor" element={<PixTransactionScreen />} />
         <Route path="/conta" element={<PixAccountScreen />} />
         <Route path="/confirmar" element={<PixConfirmationScreen />} />
         <Route path="/sucesso" element={<PixSuccessfulScreen />} />
+        <Route path="/input" element={<PixInputScreen/>} />
       </Routes>
     </Router>
   );
